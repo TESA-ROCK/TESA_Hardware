e843727b61c25c45f5b0880a9661dc0a15718d6d
RonakornST <119744243+RonakornST@users.noreply.github.com>
2024-11-12 11:25:09 +0700

    end and makefile

--- ex_03/end_thr.c
+++ ex_03/end_thr.c
@@ -16,11 +16,11 @@ void *end_thr_fcn( void *ptr ) {
     printf ( "Thread end starts at: %s", asctime (timeinfo) );
     while(1) {
         // loop
-        /* TBD */(&avg_data_cond_mutex);
-        /* TBD */(&avg_data_cond, &avg_data_cond_mutex);
+        pthread_mutex_lock(&avg_data_cond_mutex);
+        pthread_cond_wait(&avg_data_cond, &avg_data_cond_mutex);
         time (&now);
         timeinfo = localtime ( &now );
         printf("Final result: %f at %s\n", avg_shared_data, asctime (timeinfo));
-        /* TBD */(&avg_data_cond_mutex);
+        pthread_mutex_unlock(&(&avg_data_cond_mutex);
     }
 }
