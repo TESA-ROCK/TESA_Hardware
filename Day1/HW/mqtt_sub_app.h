2cfc78f20a55b70c262f1b9ba767206b12cc02c5
Fiat-Anw <fiat.anw@gmail.com>
2024-11-12 16:07:02 +0000

    add mqtt_Subscribe Homework

---
+++ Day1/HW/mqtt_sub_app.h
@@ -0,0 +1,19 @@
+#ifndef IOT_APP_H
+#define IOT_APP_H
+
+// include files
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <cjson/cJSON.h>
+
+// shared variables
+extern pthread_mutex_t data_cond_mutex;
+extern pthread_cond_t data_cond;
+extern int shared_value;
+
+// function prototypes
+void *mqtt_sub_thr_fcn(void *ptr);
+
+#endif // IOT_APP_H
