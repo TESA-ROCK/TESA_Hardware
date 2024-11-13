b2c0ad3d6d0e39a5e319f96f4dd71872453d2048
RonakornST <ultraronnakorn@gmail.com>
2024-11-13 22:36:01 +0700

    ex14 do fft by installfftw3.h

--- Day2/ex_14/fft_thr.c
+++ Day2/ex_14/fft_thr.c
@@ -1,32 +1,42 @@
 #include "sound_app.h"
 #include "sound_freq.h"
 #include <limits.h>
+#include <fftw3.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <math.h>
+
+extern pthread_mutex_t data_cond_mutex;
+extern pthread_cond_t data_cond;
+extern short shared_buf[4096];
 
 void *fft_thr_fcn(void *ptr) {
     // setup
     static double tmp_buf[4096];
-    static double freq_buf[4096];
+    static fftw_complex freq_buf[4096];
+    fftw_plan plan = fftw_plan_dft_r2c_1d(4096, tmp_buf, freq_buf, FFTW_ESTIMATE);
 
     while(1) {
         // loop
         pthread_mutex_lock(&data_cond_mutex);
         pthread_cond_wait(&data_cond, &data_cond_mutex);
         printf("Start processing\n");
-        for (int i=0; i < 4096; i++) {
-            tmp_buf[i] = (double)shared_buf[i]/SHRT_MAX;
-        }
-
-        
-        // Process FFT and store in freq_buf
         for (int i = 0; i < 4096; i++) {
-            freq_buf[i] = tmp_buf[i];  // Here, we store the normalized buffer
+            tmp_buf[i] = (double)shared_buf[i] / SHRT_MAX;
         }
 
-        // Print 3 frequency values from the spectrum
+        // Perform FFT
+        fftw_execute(plan);
+
+        // Print the magnitudes of the first three frequency values
         for (int i = 0; i < 3; i++) {
-            printf("Frequency[%d] = %.5f Hz\n", i, freq_buf[i]);  // This is an example, assuming frequency values
+            double magnitude = sqrt(freq_buf[i][0] * freq_buf[i][0] + freq_buf[i][1] * freq_buf[i][1]);
+            printf("freq_buf[%d] magnitude = %f\n", i, magnitude);
         }
 
         pthread_mutex_unlock(&data_cond_mutex);
     }
+
+    fftw_destroy_plan(plan);
+    return NULL;
 }
